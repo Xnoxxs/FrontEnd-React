4640be2a64fe53a22eee11c080c7021483e5a834
Hamza <hamzajaebak88@gmail.com>
2026-03-20 09:58:17 +0100

    Fix vite version

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
